222b0e13a4d0373edf461ee3a39c25b100f6564c
Adarsh Addee <59617563+AdarshAddee@users.noreply.github.com>
2022-10-07 19:11:48 +0530

    Create greet.java

---
+++ Java/greet.java
@@ -0,0 +1,9 @@
+// Github username: AdarshAddee
+// Aim: Greeting everyone
+// Date: 07 October 2022
+
+class greet {
+    public static void main(String[] args) {
+        System.out.println("Namaste 🙏 Bharat!"); 
+    }
+}
